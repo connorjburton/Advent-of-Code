b02bcdcf899e0f2a6e20526b1bbde93423b9aecb
Connor Burton <connor.burton@lumici.co.uk>
2015-12-14 21:36:41 +0000

    Day 2 extra done

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 let days = [
 	require('./days/one'),
 	require('./days/one-extra'),
-	require('./days/two')
+	require('./days/two'),
+	require('./days/two-extra')
 ];
 
 days.forEach(function(day, key) {
