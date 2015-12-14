7680ceab129628dfdaf7bf8aa3cf3a39bf668f46
Connor Burton <connor.burton@lumici.co.uk>
2015-12-14 19:12:52 +0000

    Day two complete

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 let days = [
-	require('./days/one')
+	require('./days/one'),
+	require('./days/two')
 ];
 
 days.forEach(function(day, key) {
-	console.log('--------------------DAY ' + (key + 1) + '--------------------');
 	new day();
 });
